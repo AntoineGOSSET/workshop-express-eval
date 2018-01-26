c83f1597e9a70d187cf95e5dee544454af275d16
AntoineGOSSET <antoine.gosset@ynov.com>
2018-01-26 15:06:36 +0100

    Exercice 1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-const app = '... please declare your Express web server here';
+var http = require ("http");
+
+const app = http.createServer(function (request, response) {
+    response.writeHead(200, {
+       'Content-Type': 'text/plain'
+    });
+    response.write('Hello World')
+    response.end();
+})
 
 module.exports = app;
